2baee543d96bad7a384e685d434d4fb711cd3b64
volkoluck74 <volko-ov@yandex.ru>
2024-12-05 15:04:50 +0000

    add gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import {program} from 'commander';
+program
+        .name('gendiff')
+        .description('Compares two configuration files and shows a difference.')
+        .version('1.0.0','-V, --version', 'output the version number')
+        .helpOption('-h, --help','output usage information');
+program.parse();
