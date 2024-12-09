771818847ef938adf9e645c058c23c5cf828d441
volkoluck74 <volko-ov@yandex.ru>
2024-12-09 14:51:27 +0000

    add format and args in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ program
         .name('gendiff')
         .description('Compares two configuration files and shows a difference.')
         .version('1.0.0','-V, --version', 'output the version number')
+        .argument('<filepath1>')
+        .argument('<filepath2>')
+        .option('-f, --format [type]', 'output format')
         .helpOption('-h, --help','output usage information');
 program.parse();
