dfc4df1b78417dbb6c53805fecc937a7587d69b7
volkoluck74 <volko-ov@yandex.ru>
2024-12-22 06:57:57 +0000

    change src/index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ import process from 'process';
 import getParseFile from './utils/parsers.js';
 import diff from './utils/diff.js';
 import formatter from './formatters/index.js';
-
+// Получаем абсолютный путь к файлу
 const getAbsFilePath = (filepath) => path.resolve(process.cwd(), filepath);
+// Получаем расширение файла
 const getFileType = (filepath) => path.extname(filepath);
+// Парсим файл, находящийся по указанному абсолютному/относительному пути
+function getData(filepath) {
+  const absFilePath = getAbsFilePath(filepath);
+  const fileType = getFileType(absFilePath);
+  const file = fs.readFileSync(absFilePath, 'utf8');
+  return getParseFile(file, fileType);
+}
 export default function gendiff(filepath1, filepath2, format) {
-  const absFilepath1 = getAbsFilePath(filepath1);
-  const absFilepath2 = getAbsFilePath(filepath2);
-  const fileType1 = getFileType(absFilepath1);
-  const fileType2 = getFileType(absFilepath2);
-  const file1 = fs.readFileSync(absFilepath1, 'utf8');
-  const file2 = fs.readFileSync(absFilepath2, 'utf8');
-  const parseFile1 = getParseFile(file1, fileType1);
-  const parseFile2 = getParseFile(file2, fileType2);
+  const parseFile1 = getData(filepath1);
+  const parseFile2 = getData(filepath2);
   const resultOfDiff = diff(parseFile1, parseFile2);
   return formatter(format)(resultOfDiff);
 }
